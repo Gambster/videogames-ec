e7274463025241f431afed3b07d5fdfd30a2455c
Gambster <gamboa040200@gmail.com>
2023-12-24 14:00:25 -0600

    fix

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -1,22 +1,26 @@
 import mongoDB, { MongoClient } from "mongodb";
 
+declare global {
+  var _mongoClientPromise: any;
+}
+
 const uri = process.env.MONGODB_URI;
 const options = {};
 let client;
-let clientPromise: MongoClient;
+let clientPromise: any;
 
 if (!uri) {
   throw new Error("Please add your mongoDB URI to your env file!");
 }
 
-client = new MongoClient(uri, options);
-
 if (process.env.NODE_ENV === "development") {
+  client = new MongoClient(uri, options);
   if (!global._mongoClientPromise) {
     global._mongoClientPromise = client.connect();
   }
   clientPromise = global._mongoClientPromise;
 } else {
+  client = new MongoClient(uri, options);
   clientPromise = client.connect();
 }
 
